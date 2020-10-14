f02d08cd155c8ade91ea12f1ea066dc5e9bfea6d
Bitdeparidad <francisjavi2@gmail.com>
2020-10-14 20:28:28 +0200

    Añadida clase cliente con una estructura básica, pendiente de implementar

---
+++ SistemasMultiagentes/src/sistemasmultiagentes/Cliente.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemasmultiagentes;
+
+/**
+ *
+ * @author bitde
+ */
+public class Cliente {
+    
+    public Cliente(){
+        
+    }
+    
+    public void funcionDelCliente(){
+        
+    }
+    
+    private String mensajeAltaMonitor(){
+        return "por hacer";
+    }
+    
+    private String mensajeAltaTienda(){
+        return "por hacer";
+    }
+    
+    private String mensajeConsultaProductos(){
+        return "por hacer";
+    }
+    
+    private String mensajeCompraProductos(){
+        return "por hacer";
+    }
+    
+    private String mensajeConsultaClientes(){
+        return "por hacer";
+    }
+    
+    private String mensajeConsultaTiendas(){
+        return "por hacer";
+    }
+    
+    private String mensajeBajaTiendas(){
+        return "por hacer";
+    }
+    
+    
+}
