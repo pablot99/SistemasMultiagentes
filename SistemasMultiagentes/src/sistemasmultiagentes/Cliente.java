a148d9ae3e8344fd488e7320eaff4f3c12637ed5
PABLO TORRIJOS ARENAS <Pablo.Torrijos@alu.uclm.es>
2020-10-20 10:38:38 +0200

    Constructor de Cliente y clase Productos

--- SistemasMultiagentes/src/sistemasmultiagentes/Cliente.java
+++ SistemasMultiagentes/src/sistemasmultiagentes/Cliente.java
@@ -5,14 +5,36 @@
  */
 package sistemasmultiagentes;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author bitde
  */
 public class Cliente {
     
-    public Cliente(){
-        
+    private static int id;                      // ID del Cliente
+    private static String ipMonitor;            // IP del Monitor
+    private static ArrayList<String> tiendas;   // Tiendas conocidas por el Cliente
+    private static Productos productos;         // Productos a comprar
+    
+    
+    /**
+     * Constructor para el cliente. Inicializa las variables y obtiene del Monitor
+     * el ID del Cliente, la Lista de Productos y 2 Tiendas conocidas.
+     * @param ip IP del Monitor
+    */
+    public Cliente(String ip){
+        // Inicializaciones
+        ipMonitor = ip;
+	tiendas = new ArrayList<>();
+	
+        /* Se pide al monitor el ID, la Lista de Productos y las Tiendas
+         * conocidas mediante el mensaje "mensajeAltaMonitor()". */
+        String respuesta = mensajeAltaMonitor();
+        id = respuesta[0];
+        productos = new Productos(respuesta[1]);
+        tiendas = respuesta[2];
     }
     
     public void funcionDelCliente(){
