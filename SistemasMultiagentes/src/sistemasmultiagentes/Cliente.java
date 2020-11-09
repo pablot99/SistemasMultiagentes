efcd82ac002a07715fd71419bb4e602cf91c1ecc
Bitdeparidad <francisjavi2@gmail.com>
2020-11-09 12:07:49 +0100

    Modificados metodos de mensajes para que devuelvan directamente el DoM. En relacion con #1 #16 #20 #21 #22 #23 #24 #25

--- SistemasMultiagentes/src/sistemasmultiagentes/Cliente.java
+++ SistemasMultiagentes/src/sistemasmultiagentes/Cliente.java
@@ -49,11 +49,11 @@ public class Cliente {
     public void funcionDelCliente(){
         while(!productos.isEmpty()){
             //Me doy de alta en la tienda
-            Document respuestaAltaTienda = xmlToDom(mensajeAltaTienda());
+            Document respuestaAltaTienda = mensajeAltaTienda();
             System.out.println(respuestaAltaTienda);
             
             //Pido catalogo
-            Document respuestaConsultaProductos = xmlToDom(mensajeConsultaProductos());
+            Document respuestaConsultaProductos = mensajeConsultaProductos();
             System.out.println(respuestaConsultaProductos);
             Productos catalogo = new Productos(respuestaConsultaProductos);
             
@@ -62,7 +62,7 @@ public class Cliente {
                 //si el producto esta en la lista de la compra
                 if(productos.getProductos().containsKey(par.getKey())){
                     //Compro un producto
-                    Document respuestaCompraProducto = xmlToDom(mensajeCompraProductos());
+                    Document respuestaCompraProducto = mensajeCompraProductos();
                     System.out.println(respuestaCompraProducto);
                     //Resto cuanto he comprado
                     //Quito producto de la lista si ya he comprado todo
@@ -71,18 +71,18 @@ public class Cliente {
             }
             
             //Pedimos la lista de clientes que hay en la tienda
-            Document respuestaConsultaClientes = xmlToDom(mensajeConsultaClientes());
+            Document respuestaConsultaClientes = mensajeConsultaClientes();
             System.out.println(respuestaConsultaClientes);
             ArrayList<Integer> clientes = new ArrayList<>();
                 ///leer la respuesta y escribirla en la lista
             for(int cliente: clientes){
-                Document respuestaConsultaTiendas = xmlToDom(mensajeConsultaTiendas());
+                Document respuestaConsultaTiendas = mensajeConsultaTiendas();
                 System.out.println(respuestaConsultaTiendas);
                 //añadimos tiendas a la lista
             }
             
             //Nos damos de baja en la tienda y vamos a la sigiente
-            Document respuestaBajaTienda = xmlToDom(mensajeBajaTiendas());
+            Document respuestaBajaTienda = mensajeBajaTiendas();
             System.out.println(respuestaBajaTienda);
         }
     }
@@ -91,7 +91,7 @@ public class Cliente {
     // como aún no podemos recibir mensajes de la tienda, leemos de un fichero de texto
     // mensajes que hemos escrito
     
-    private File mensajeAltaMonitor() throws FileNotFoundException{
+    private Document mensajeAltaMonitor() throws FileNotFoundException{
         
 
         // Creamos un string con la respuesta que recibimos
@@ -103,65 +103,65 @@ public class Cliente {
         // Para hacer una prueba leemos de un fichero que hemos creado
         
         File fichero = new File("./project/nbproject/respuestas/RespuestaAltaMonitor.txt");
-        return fichero;
+        return xmlToDom(fichero);
     }
     
-    private File mensajeAltaTienda(){
+    private Document mensajeAltaTienda(){
         
         // Mismos pasos iniciales
         // Para darnos de alta en la tienda necesitamos su IP y pedir permiso al monitor
         // Recibiremos un mensaje de la tienda que nos indique que nos hemos dado de alta correctamente
         
         File fichero = new File("./project/nbproject/respuestas/RespuestaAltaTienda.txt");
-        return fichero;
+        return xmlToDom(fichero);
         
     }
     
-    private File mensajeConsultaProductos(){
+    private Document mensajeConsultaProductos(){
         
         // Mismos pasos iniciales de antes
         // Consultamos a la tienda la lista de todos sus productos
         
         File fichero = new File("./project/nbproject/respuestas/RespuestaListaProductos.txt");
-        return fichero;
+        return xmlToDom(fichero);
     }
     
-    private File mensajeCompraProductos(){
+    private Document mensajeCompraProductos(){
         
         // Mismos pasos iniciales de antes
         // Cuando hayamos consultado los productos que tiene la tienda, le indicamos cuales queremos comprar
         // (todos los que tengan de nuestra lista)
         
         File fichero = new File("./project/nbproject/respuestas/RespuestaComprarProductos.txt");
-        return fichero;
+        return xmlToDom(fichero);
     }
     
-    private File mensajeConsultaClientes(){
+    private Document mensajeConsultaClientes(){
         
         // Mismos pasos iniciales de antes
         // Pedimos a la tienda los Id de todos los clientes que están en la tienda
         
         File fichero = new File("./project/nbproject/respuestas/RespuestaListaClientes.txt");
-        return fichero;
+        return xmlToDom(fichero);
     }
     
-    private File mensajeConsultaTiendas(){
+    private Document mensajeConsultaTiendas(){
         
         // Mismos pasos iniciales de antes
         // Pedimos a la tienda las tiendas que conozca un cliente conociendo su Id
         
         File fichero = new File("./project/nbproject/respuestas/RespuestaListaTiendas.txt");
-        return fichero;
+        return xmlToDom(fichero);
     }
     
-    private File mensajeBajaTiendas(){
+    private Document mensajeBajaTiendas(){
         
         // Mismos pasos iniciales
         // Para darnos de baja en la tienda necesitamos su IP y pedir permiso al monitor
         // Recibiremos un mensaje de la tienda que nos indique que nos hemos dado de baja correctamente
         
         File fichero = new File("./project/nbproject/respuestas/RespuestaBajaTiendas.txt");
-        return fichero;
+        return xmlToDom(fichero);
     }
     
     private Document xmlToDom(File f){
